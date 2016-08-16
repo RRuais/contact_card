6b04da44140627908280cd4316977ec0e07717ff
Rich Ruais <richruais@gmail.com>
2016-08-16 16:41:13 -0700

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+  $(document).ready(function () {
+      
+      
+//      $(".img-responsive").click(function () {
+//          $(this).hide();
+//      });
+//      
+//       $("#reset").click(function () {
+//          $(".img-responsive").show();
+//      });
+   
+  });
