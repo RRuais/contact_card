f8d673c75cf34235f14f7770b3e1b0a071c7286a
Rich Ruais <richruais@gmail.com>
2016-08-16 20:39:50 -0700

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
   $(document).ready(function () {
-      
-      
-//      $(".img-responsive").click(function () {
-//          $(this).hide();
-//      });
-//      
-//       $("#reset").click(function () {
-//          $(".img-responsive").show();
-//      });
-   
+      function add_description() {
+          $("h3").click(function () {
+              $(".contact_card p").toggle();
+          });
+      }
+      $("form").submit(function () {
+          var first = $('#first_name').val();
+          var last = $('#last_name').val();
+          var descr = $('#description').val();
+          $("#right_body").append("<div class='contact_card'>" + "<h1>" + first + " " + last + "</h1>" + "<h3>Click Here For Description!</h3>" + "<p>" + descr + "</p>" + "</div>");
+          $(".contact_card p").hide();
+          
+          //clears inputs
+          $("#first_name").val("");
+          $("#last_name").val("");
+          $("#description").val("");
+          
+          add_description();
+          return false;
+      });
   });
